c7fe7096f7aba3d925eda790e7f6f04a40b6ff75
rubiotorres <rubiotorres15@gmail.com>
2019-04-02 10:13:19 -0300

    PR final

--- cliente.c
+++ cliente.c
@@ -9,38 +9,50 @@
 #include <sys/mman.h>
 #include<math.h>
 int main(){
-//Inicializa variaves
+	//Inicializa variaves
 	const char *name = "shared_memory";
 	const int SIZE = pow(1024,3);
+
 	int shm_fd;
-	char *start,*s;
-	char *ptr;
-	int i,j,cont=0;
-//Inicializa leitura/escrita da memoria
+	int *start;
+	int *ptr;
+	int i,j;
+	long int cont=0;
+
+	//Inicializa leitura/escrita da memoria
 	shm_fd = shm_open(name, O_RDWR, 0666);
 	if (shm_fd == -1) {
 		printf("shared memory failed\n");
 		exit(-1);
 	}
-	ptr = mmap(0,SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+
+	//Mapeia memória
+	ptr = mmap(0,SIZE*sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
 	if (ptr == MAP_FAILED) {
 		printf("Map failed\n");
 		exit(-1);
 	}
-//Salva começo da memoria
+
+	//Salva começo da memoria
     start=ptr;
-    ptr++;
-//Lê a memoria e faz a comparação
-	for(j=1;ptr[j]!='\0';j++){
-        while(ptr[j]==-1){
-            j++;
-        }
-		if(ptr[j]==*start){			
+
+	//"Pula" os dois primeiros termos
+    ptr+=2;
+
+	//Lê a memoria
+	for(j=3;(char)ptr[j]!='\0';j++){
+
+		//Faz comparação
+		if((char)ptr[j]==(char)*start){			
 			cont++;
 		}	
 	}
-// Escreve resultado na memoria
-	start[1]=cont;
-    printf("Nesse processo foi lido %d termos '%c'\n",cont,*start);
+	
+	// Escreve resultado na memoria
+	*(start+1)=(long int)cont;
+
+	//Printa o resultado local
+	printf("Contagem registrada.\n");
+
 	return 0;
 }
--- servidor.c
+++ servidor.c
@@ -1,5 +1,6 @@
 /***********Processo "servidor" da memória compartilhada
 *****Autor: Rubio Torres*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,52 +13,76 @@
 #include <unistd.h>
 
 int main(){
-/*CRIAÇÃO DA MEMORIA COM 1GB COMO CREATOR OU READ & WRITE*/
-	const int SIZE = pow(902,3);
+
+	const int SIZE = pow(1024,3);
 	const char *name = "shared_memory";
-    char a;
-	int shm_fd,tamanho,contador=0;
-	char *ptr, *start;
+
+    char char_requerido;
+	int shm_fd,tamanho,contador=0;//contadora=0;
+	int *ptr, *start;
+    
+    //ABERTURA DA MEMORIA COMO CREATOR OU READ & WRITE
 	shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
-	ftruncate(shm_fd,SIZE);
-    ptr = mmap(0,SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+
+	ftruncate(shm_fd,1 + sizeof(int)*SIZE);
+
+    //Mapear memória
+    ptr = mmap(0,sizeof(int)*SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
 	if (ptr == MAP_FAILED) {
 		printf("Map failed\n");
 		return -1;
 	}
-/*SALVANDO A POSIÇÃO INICIAL DA MEMORIA*/
+
+    //SALVANDO A POSIÇÃO INICIAL DA MEMORI
 	start = ptr;
-/*AJUSTANDO AS VARIAVEIS A FIM DE PESQUISA*/
+
+    //AJUSTANDO AS VARIAVEIS A FIM DE PESQUISA
     printf("Qual o char: ");
-	scanf("%c",ptr);
-    a= *ptr;
-    ptr++;
-	printf("Quantos processos: ");
+	scanf("%c",&char_requerido);
+    printf("Quantos processos: ");
 	scanf("%d",&tamanho);
+    *ptr=(char)char_requerido;
+    ptr++;
     *ptr=-1;
     ptr++;
-/*ESCREVE NA MEMORIA*/
-    for(int i=1;i<=tamanho;i++){
-        for(float j=((SIZE/tamanho)*(i-1));j<ceil(((SIZE/tamanho)*(i)));j++,ptr++ ){
-            *ptr=97 + rand() % 26;
-        }
-        *ptr = '\0';
-/*VERIFICA SE JA DEU RESPOSTA*/
+
+    //ESCREVE NA MEMORIA
+    for(int j=0,i=1, processo=1;j<SIZE+1;j++,ptr++,i++ ){
+        *ptr=(char)97 + rand() % 26;
+    /*-----------------------------------------------------------
+        Verifica resposta
+        if(*ptr==char_requerido){
+            contadora++;
+        } 
+    -------------------------------------------------------------*/
+
+        //Faz controle da divisão da memoria
+        if(j==(SIZE/tamanho)*processo){
+            printf("Esperando processo...\n");
+            processo++;
+        
+        //Espera o processo responder
             while(1){
-                if(start[1]>=0){
-                    contador+=start[1];
+                *ptr='\0';
+                if(start[1]!=-1){
+
+                    //Soma resposta
+                    contador+=(long int)start[1];
+
+                    //Reinicia memoria             
+                    ptr=start;
+                    *ptr=(char)char_requerido;
+                    ptr++;
+                    *ptr=-1;
+                    ptr++;
                     break;
-                }
-            } 
-/*REINICIA A ESCRITA DA MEMORIA*/   	
-        ptr=start;
-        *ptr=a;
-        ptr++;
-        *ptr=-1;
-        ptr++;
+                } 
+            }
+        }
     }
-/*MOSTRA A RESPOSTA*/
-    printf("A letra '%c' teve %d ocorrências\n",a, contador);
+
+    //MOSTRA A RESPOSTA
+    printf("A letra '%c' teve %d ocorrência(s)\n",char_requerido, contador);
 
 	return 0;
 }
